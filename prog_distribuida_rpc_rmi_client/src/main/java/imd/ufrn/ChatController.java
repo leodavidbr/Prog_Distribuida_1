8c65a550c0cf902ada1cd3c59ceb5a3daaf3b68d
leodavidbr <rbdivadoel@gmail.com>
2024-03-20 23:53:15 -0300

    Base project of Client and Service. The implementation for BaseCommunicationWithServer needs to be provided.

--- prog_distribuida_rpc_rmi_client/src/main/java/imd/ufrn/ChatController.java
+++ prog_distribuida_rpc_rmi_client/src/main/java/imd/ufrn/ChatController.java
@@ -1,21 +1,32 @@
 package imd.ufrn;
 
 import imd.ufrn.interfaces.IChatPresenter;
-import imd.ufrn.interfaces.ICommunicationWithServerController;
+import imd.ufrn.interfaces.BaseCommunicationWithServerController;
+import imd.ufrn.interfaces.BaseInputReceiver;
 
-public class ChatController implements Runnable {
-    ICommunicationWithServerController serverCommunicationController;
+// controller could be a thread (runnable) that saves the values sent by the callback and unlocks a concurrent lock
+// that was locking the execution of the specific handle method (I feel that it would need a little bit more complexity than described)
+// this way, all the logic is done in the ChatController thread without stoping the other threads for 
+// the execution of the processing
+// but that is a lot of unnecessary complexity for this project.
+public class ChatController {
+    BaseCommunicationWithServerController serverCommunicationController; // Maybe there's no need to save this
+    BaseInputReceiver inputReceiver; // Maybe there's no need to save this
+    Thread serverCommunicationControllerThread;
+    Thread inputRecieverThread;
     IChatPresenter chatPresenter;
 
     public ChatController() {
-        serverCommunicationController = new rpcRmiCommunicationController();
-        serverCommunicationController.initialize(serverMessage -> handleMessageRecievedFromServer(serverMessage));
+        serverCommunicationController = new SocketCommunicationController(
+                serverMessage -> handleMessageRecievedFromServer(serverMessage));
+        inputReceiver = new InputReceiverTerminalImpl(
+                message -> handleSendMessageFromClient(message));
         chatPresenter = new ChatPresenterTerminalImpl();
-    }
 
-    @Override
-    public void run() {
-
+        serverCommunicationControllerThread = new Thread(serverCommunicationController);
+        inputRecieverThread = new Thread(inputReceiver);
+        serverCommunicationControllerThread.start();
+        inputRecieverThread.start();
     }
 
     public void handleSendMessageFromClient(String clientMessage) {
